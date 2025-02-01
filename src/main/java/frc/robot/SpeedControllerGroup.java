9127af0fd43873b210874a766ff3af859fe5efa5
SFIHS Robotics <sfihsrobotics@gmail.com>
2025-02-01 14:02:15 -0800

    first commit

---
+++ src/main/java/frc/robot/SpeedControllerGroup.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+
+public class SpeedControllerGroup {
+
+    public SpeedControllerGroup(PWMSparkMax rightMotorSpark, PWMSparkMax backRightMotor) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
